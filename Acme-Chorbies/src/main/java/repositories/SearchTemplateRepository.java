dcd20d3539989995b71a04d8c200aeb5108b96ad
niclorros <n1c0l0r3nz69@gmail.com>
2017-04-03 12:16:26 +0200

    Comienzo el SearchTemplate

--- Acme-Chorbies/src/main/java/repositories/SearchTemplateRepository.java
+++ Acme-Chorbies/src/main/java/repositories/SearchTemplateRepository.java
@@ -1,3 +1,4 @@
+
 package repositories;
 
 import java.util.Collection;
@@ -6,15 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import security.UserAccount;
 import domain.SearchTemplate;
 
 @Repository
-public interface SearchTemplateRepository extends JpaRepository<SearchTemplate, Integer>{
+public interface SearchTemplateRepository extends JpaRepository<SearchTemplate, Integer> {
 
 	@Query("select s from SearchTemplate s where s.genre.id = ?1")
 	Collection<SearchTemplate> findSearchTemplateByGenre(int genre);
-	
+
 	@Query("select s from SearchTemplate s where s.kindRelationship.id = ?1")
 	Collection<SearchTemplate> findSearchTemplateByKindRelationship(int kindRelationship);
-	
+
+	@Query("select c.searchTemplate from Chorbi c where c.userAccount = ?1")
+	SearchTemplate findByUserAccount(UserAccount userAccount);
+
 }
--- Acme-Chorbies/src/main/java/services/SearchTemplateService.java
+++ Acme-Chorbies/src/main/java/services/SearchTemplateService.java
@@ -1,5 +1,7 @@
+
 package services;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +10,21 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.SearchTemplateRepository;
+import security.LoginService;
+import security.UserAccount;
+import domain.Chorbi;
 import domain.SearchTemplate;
 
 @Service
 @Transactional
 public class SearchTemplateService {
-	
+
 	// Managed repository -----------------------------------------------------
-	
+
 	@Autowired
-	private SearchTemplateRepository searchTemplateRepository;
-	
+	private SearchTemplateRepository	searchTemplateRepository;
+
+
 	// Supporting services ----------------------------------------------------
 
 	// Constructors -----------------------------------------------------------
@@ -28,11 +34,15 @@ public class SearchTemplateService {
 	}
 
 	// Simple CRUD methods ----------------------------------------------------
-			
+
 	public SearchTemplate create() {
 
 		SearchTemplate result;
 		result = new SearchTemplate();
+
+		Collection<Chorbi> chorbies = new ArrayList<Chorbi>();
+
+		result.setChorbies(chorbies);
 
 		return result;
 	}
@@ -69,20 +79,45 @@ public class SearchTemplateService {
 	public void delete(SearchTemplate searchTemplate) {
 
 		Assert.notNull(searchTemplate);
-				
+
 		Assert.isTrue(searchTemplate.getId() != 0);
 
 		searchTemplateRepository.delete(searchTemplate);
 	}
-	
+
 	// Other bussiness methods ------------------------------------------------
 
-	public Collection<SearchTemplate> findSearchTemplateByGenre(int genre){
+	public Collection<SearchTemplate> findSearchTemplateByGenre(int genre) {
 		return searchTemplateRepository.findSearchTemplateByGenre(genre);
 	}
-	
-	public Collection<SearchTemplate> findSearchTemplateByKindRelationship(int kindRelationship){
+
+	public Collection<SearchTemplate> findSearchTemplateByKindRelationship(int kindRelationship) {
 		return searchTemplateRepository.findSearchTemplateByKindRelationship(kindRelationship);
 	}
-	
+
+	public Boolean compareSearch(SearchTemplate st) {
+		SearchTemplate old = findByPrincipal();
+		Boolean res = false;
+		if (old.getAge().compareTo(st.getAge()) != 0 
+			&& old.getKeyword().compareTo(st.getKeyword()) != 0 
+			&& old.getCoordinate().getCity().compareTo(st.getCoordinate().getCity()) != 0 
+			&& old.getCoordinate().getCountry().compareTo(st.getCoordinate().getCountry()) != 0
+			&& old.getCoordinate().getProvince().compareTo(st.getCoordinate().getProvince()) != 0
+			&& old.getCoordinate().getState().compareTo(st.getCoordinate().getState()) != 0
+			&& old.getKindRelationship().getValue().compareTo(st.getKindRelationship().getValue()) != 0
+			&& old.getGenre().getValue().compareTo(st.getGenre().getValue()) != 0)
+			res = true;
+		return res;
+	}
+
+	public SearchTemplate findByPrincipal() {
+		SearchTemplate result;
+		UserAccount userAccount;
+
+		userAccount = LoginService.getPrincipal();
+		result = searchTemplateRepository.findByUserAccount(userAccount);
+
+		return result;
+	}
+
 }
