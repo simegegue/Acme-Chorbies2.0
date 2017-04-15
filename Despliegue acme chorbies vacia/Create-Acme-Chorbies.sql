a2b24bc6b6edcc0597f36466c8c608f055bd6ebb
Felipe Javier Garcia da Silva <felgarda@alum.us.es>
2017-04-15 10:35:42 +0200

    Creados los scripts de base de datos vacía

---
+++ Despliegue acme chorbies vacia/Create-Acme-Chorbies.sql
@@ -0,0 +1,488 @@
+start transaction;
+
+drop database if exists `Acme-Chorbies`;
+create database `Acme-Chorbies`;
+
+use `Acme-Pad-Thai`;
+
+create user 'acme-user'@'%' identified by password '*4F10007AADA9EE3DBB2CC36575DFC6F4FDE27577';
+create user 'acme-manager'@'%' identified by password '*FDB8CD304EB2317D10C95D797A4BD7492560F55F';
+
+grant select, insert, update, delete
+	on `Acme-Chorbies`.* to 'acme-user'@'%';
+
+grant select, insert, update, delete, create, drop, references, index, alter, 
+        create temporary tables, lock tables, create view, create routine, 
+        alter routine, execute, trigger, show view
+	 on `Acme-Chorbies`.* to 'acme-manager'@'%';
+
+-- MySQL dump 10.13  Distrib 5.5.29, for Win64 (x86)
+--
+-- Host: localhost    Database: acme-chorbies
+-- ------------------------------------------------------
+-- Server version	5.5.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actor`
+--
+
+DROP TABLE IF EXISTS `actor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actor` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `userAccount_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_cgls5lrufx91ufsyh467spwa3` (`userAccount_id`),
+  CONSTRAINT `FK_cgls5lrufx91ufsyh467spwa3` FOREIGN KEY (`userAccount_id`) REFERENCES `useraccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actor`
+--
+
+LOCK TABLES `actor` WRITE;
+/*!40000 ALTER TABLE `actor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `administrator`
+--
+
+DROP TABLE IF EXISTS `administrator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `userAccount_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_idt4b4u259p6vs4pyr9lax4eg` (`userAccount_id`),
+  CONSTRAINT `FK_idt4b4u259p6vs4pyr9lax4eg` FOREIGN KEY (`userAccount_id`) REFERENCES `useraccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+INSERT INTO `administrator` VALUES (26,0,'manuto@gmail.com','Manuel','+34 965845789','Torres',14);
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `banner`
+--
+
+DROP TABLE IF EXISTS `banner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `banner`
+--
+
+LOCK TABLES `banner` WRITE;
+/*!40000 ALTER TABLE `banner` DISABLE KEYS */;
+INSERT INTO `banner` VALUES (15,0,'https://c1.staticflickr.com/3/2813/33640539192_f498ce27f2.jpg'),(16,0,'https://c1.staticflickr.com/4/3837/33667275001_890d00ddd7.jpg'),(17,0,'https://c1.staticflickr.com/4/3856/33667279331_a4ec3acb2e_b.jpg');
+/*!40000 ALTER TABLE `banner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cachetime`
+--
+
+DROP TABLE IF EXISTS `cachetime`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cachetime` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cachetime`
+--
+
+LOCK TABLES `cachetime` WRITE;
+/*!40000 ALTER TABLE `cachetime` DISABLE KEYS */;
+INSERT INTO `cachetime` VALUES (18,0,'12:00:00');
+/*!40000 ALTER TABLE `cachetime` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chirp`
+--
+
+DROP TABLE IF EXISTS `chirp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chirp` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `deleteRecipient` bit(1) NOT NULL,
+  `deleteSender` bit(1) NOT NULL,
+  `moment` datetime DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `text` varchar(255) DEFAULT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_20arsrvo5x8fqqrj7o5tomu6f` (`recipient_id`),
+  KEY `FK_7nnun85ngke6yiv2qnerep5uc` (`sender_id`),
+  CONSTRAINT `FK_7nnun85ngke6yiv2qnerep5uc` FOREIGN KEY (`sender_id`) REFERENCES `chorbi` (`id`),
+  CONSTRAINT `FK_20arsrvo5x8fqqrj7o5tomu6f` FOREIGN KEY (`recipient_id`) REFERENCES `chorbi` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chirp`
+--
+
+LOCK TABLES `chirp` WRITE;
+/*!40000 ALTER TABLE `chirp` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chirp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chirp_attachment`
+--
+
+DROP TABLE IF EXISTS `chirp_attachment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chirp_attachment` (
+  `Chirp_id` int(11) NOT NULL,
+  `attachment` varchar(255) DEFAULT NULL,
+  KEY `FK_dd8t51u0hqax65l7gq4p6gsn3` (`Chirp_id`),
+  CONSTRAINT `FK_dd8t51u0hqax65l7gq4p6gsn3` FOREIGN KEY (`Chirp_id`) REFERENCES `chirp` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chirp_attachment`
+--
+
+LOCK TABLES `chirp_attachment` WRITE;
+/*!40000 ALTER TABLE `chirp_attachment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chirp_attachment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chorbi`
+--
+
+DROP TABLE IF EXISTS `chorbi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chorbi` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `surname` varchar(255) DEFAULT NULL,
+  `userAccount_id` int(11) NOT NULL,
+  `banned` bit(1) DEFAULT NULL,
+  `birthDate` date DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `brandName` varchar(255) DEFAULT NULL,
+  `cvv` int(11) DEFAULT NULL,
+  `expirationMonth` int(11) DEFAULT NULL,
+  `expirationYear` int(11) DEFAULT NULL,
+  `holderName` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `genre_id` int(11) NOT NULL,
+  `kindRelationship_id` int(11) NOT NULL,
+  `searchTemplate_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_5louq4cq51fl6wldjwd5ivlw2` (`searchTemplate_id`),
+  UNIQUE KEY `UK_qrvmwkp25xc5exr6m3jgaxu4x` (`userAccount_id`),
+  KEY `FK_26bw2hbx9ok56nvixdljxo6ga` (`genre_id`),
+  KEY `FK_gxmf3h253pkfjjmdju64d6tuo` (`kindRelationship_id`),
+  CONSTRAINT `FK_qrvmwkp25xc5exr6m3jgaxu4x` FOREIGN KEY (`userAccount_id`) REFERENCES `useraccount` (`id`),
+  CONSTRAINT `FK_26bw2hbx9ok56nvixdljxo6ga` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`),
+  CONSTRAINT `FK_5louq4cq51fl6wldjwd5ivlw2` FOREIGN KEY (`searchTemplate_id`) REFERENCES `searchtemplate` (`id`),
+  CONSTRAINT `FK_gxmf3h253pkfjjmdju64d6tuo` FOREIGN KEY (`kindRelationship_id`) REFERENCES `kindrelationship` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chorbi`
+--
+
+LOCK TABLES `chorbi` WRITE;
+/*!40000 ALTER TABLE `chorbi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chorbi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `genre`
+--
+
+DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `genre` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_6eoi7r1ljqg0tgtackcxhpwxg` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `genre`
+--
+
+LOCK TABLES `genre` WRITE;
+/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
+INSERT INTO `genre` VALUES (19,0,'none'),(20,0,'man'),(21,0,'woman');
+/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequences`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hibernate_sequences` (
+  `sequence_name` varchar(255) DEFAULT NULL,
+  `sequence_next_hi_value` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequences`
+--
+
+LOCK TABLES `hibernate_sequences` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequences` DISABLE KEYS */;
+INSERT INTO `hibernate_sequences` VALUES ('DomainEntity',1);
+/*!40000 ALTER TABLE `hibernate_sequences` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kindrelationship`
+--
+
+DROP TABLE IF EXISTS `kindrelationship`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kindrelationship` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `value` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kindrelationship`
+--
+
+LOCK TABLES `kindrelationship` WRITE;
+/*!40000 ALTER TABLE `kindrelationship` DISABLE KEYS */;
+INSERT INTO `kindrelationship` VALUES (22,0,'none'),(23,0,'activities'),(24,0,'friendship'),(25,0,'love');
+/*!40000 ALTER TABLE `kindrelationship` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `relationlike`
+--
+
+DROP TABLE IF EXISTS `relationlike`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `relationlike` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `moment` datetime DEFAULT NULL,
+  `likeRecipient_id` int(11) NOT NULL,
+  `likeSender_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_ph7kqessbmytcwaagchn7p01a` (`likeRecipient_id`),
+  KEY `FK_h3m08tdtc2dbryq7kgm66s3ag` (`likeSender_id`),
+  CONSTRAINT `FK_h3m08tdtc2dbryq7kgm66s3ag` FOREIGN KEY (`likeSender_id`) REFERENCES `chorbi` (`id`),
+  CONSTRAINT `FK_ph7kqessbmytcwaagchn7p01a` FOREIGN KEY (`likeRecipient_id`) REFERENCES `chorbi` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `relationlike`
+--
+
+LOCK TABLES `relationlike` WRITE;
+/*!40000 ALTER TABLE `relationlike` DISABLE KEYS */;
+/*!40000 ALTER TABLE `relationlike` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `searchtemplate`
+--
+
+DROP TABLE IF EXISTS `searchtemplate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `searchtemplate` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `keyword` varchar(255) DEFAULT NULL,
+  `lastTimeSearched` datetime DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL,
+  `kindRelationship_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_jbll5vcngm57qji2jm39kyvsr` (`genre_id`),
+  KEY `FK_j8qcxj55yqmgigifjqvei8yv2` (`kindRelationship_id`),
+  CONSTRAINT `FK_j8qcxj55yqmgigifjqvei8yv2` FOREIGN KEY (`kindRelationship_id`) REFERENCES `kindrelationship` (`id`),
+  CONSTRAINT `FK_jbll5vcngm57qji2jm39kyvsr` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `searchtemplate`
+--
+
+LOCK TABLES `searchtemplate` WRITE;
+/*!40000 ALTER TABLE `searchtemplate` DISABLE KEYS */;
+/*!40000 ALTER TABLE `searchtemplate` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `searchtemplate_chorbi`
+--
+
+DROP TABLE IF EXISTS `searchtemplate_chorbi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `searchtemplate_chorbi` (
+  `SearchTemplate_id` int(11) NOT NULL,
+  `chorbies_id` int(11) NOT NULL,
+  KEY `FK_3t03s45pt13r22kb7y510060k` (`chorbies_id`),
+  KEY `FK_8qqojnmpd0r5ur1bo34ymb973` (`SearchTemplate_id`),
+  CONSTRAINT `FK_8qqojnmpd0r5ur1bo34ymb973` FOREIGN KEY (`SearchTemplate_id`) REFERENCES `searchtemplate` (`id`),
+  CONSTRAINT `FK_3t03s45pt13r22kb7y510060k` FOREIGN KEY (`chorbies_id`) REFERENCES `chorbi` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `searchtemplate_chorbi`
+--
+
+LOCK TABLES `searchtemplate_chorbi` WRITE;
+/*!40000 ALTER TABLE `searchtemplate_chorbi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `searchtemplate_chorbi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `useraccount`
+--
+
+DROP TABLE IF EXISTS `useraccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `useraccount` (
+  `id` int(11) NOT NULL,
+  `version` int(11) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_csivo9yqa08nrbkog71ycilh5` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `useraccount`
+--
+
+LOCK TABLES `useraccount` WRITE;
+/*!40000 ALTER TABLE `useraccount` DISABLE KEYS */;
+INSERT INTO `useraccount` VALUES (14,0,'21232f297a57a5a743894a0e4a801fc3','admin');
+/*!40000 ALTER TABLE `useraccount` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `useraccount_authorities`
+--
+
+DROP TABLE IF EXISTS `useraccount_authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `useraccount_authorities` (
+  `UserAccount_id` int(11) NOT NULL,
+  `authority` varchar(255) DEFAULT NULL,
+  KEY `FK_b63ua47r0u1m7ccc9lte2ui4r` (`UserAccount_id`),
+  CONSTRAINT `FK_b63ua47r0u1m7ccc9lte2ui4r` FOREIGN KEY (`UserAccount_id`) REFERENCES `useraccount` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `useraccount_authorities`
+--
+
+LOCK TABLES `useraccount_authorities` WRITE;
+/*!40000 ALTER TABLE `useraccount_authorities` DISABLE KEYS */;
+INSERT INTO `useraccount_authorities` VALUES (14,'ADMIN');
+/*!40000 ALTER TABLE `useraccount_authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-15 10:30:25
+
+commit;
